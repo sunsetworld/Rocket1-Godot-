0605034447ffca86dcf0951a2ec3febf3b2335c8
Samuel Cox/sunsetworld <68568350+sunsetworld@users.noreply.github.com>
2022-08-27 10:09:12 +0100

    Update Node2D.cs

--- Node2D.cs
+++ Node2D.cs
@@ -10,7 +10,9 @@ public class Node2D : Godot.Node2D
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        GD.Print("Hello World");
+        GD.Print("Hello World!");
+        GD.Print("こんにちはせかい！");
+        
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
